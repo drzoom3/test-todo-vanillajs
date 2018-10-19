806c8ac1feb50a7659db4361fcbd8c0291f96e12
drz <zoom.site.design@gmail.com>
2018-10-19 11:51:04 +0500

    Test deploy

--- index.php
+++ index.php
@@ -1,1 +1,86 @@
-<?php header( 'Location: /index.html' ) ;  ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ToDo App</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/script.js"></script>
+  </head>
+  <body>
+      <div class="content">
+            <div class="header clearfix">
+                <div class="pull-left">
+                    <select class="form-control" id="select-type" onchange="app.selectType(this.value)">
+                        <option value="" default>Select Data</option>
+                        <option value="contacts">Contacts</option>
+                        <option value="persons">Persons</option>
+                        <option value="deals">Deals</option>
+                        <option value="events">Events</option>
+                    </select>
+                </div>            
+                <div class="pull-right">
+                    <button id="button-add" type="button" onclick="app.addItem()" class="btn hide">Add</button>
+                    <button id="button-list" type="button" onclick="app.showList()" class="btn hide">Show List</button>
+                </div>
+            </div>
+            <form name="form-contacts" class="form hide">
+                <div class="form-row">
+                    <label class="form-label">Title</label>
+                    <input class="form-control" type="text" name="title" />
+                </div>
+                <div class="form-row">
+                    <label class="form-label">Description</label>
+                    <textarea class="form-control" name="description" ></textarea>
+                </div>
+            </form>
+            <form name="form-persons" class="form hide">
+                <div class="form-row">
+                    <label class="form-label">Name</label>
+                    <input class="form-control" type="text" name="name" />
+                </div>
+                <div class="form-row">
+                    <label class="form-label">Description</label>
+                    <textarea class="form-control" name="description" ></textarea>
+                </div>
+            </form>
+            <form name="form-deals" class="form hide">
+                <div class="form-row">
+                    <label class="form-label">Title</label>
+                    <input class="form-control" type="text" name="title" />
+                </div>
+                <div class="form-row">
+                    <label class="form-label">Description</label>
+                    <textarea class="form-control" name="description" ></textarea>
+                </div>
+                <div class="form-row">
+                    <label class="form-label">Company</label>
+                    <input class="form-control" type="text" name="company" />
+                </div>
+                <div class="form-row">
+                    <label class="form-label">Email</label>
+                    <input class="form-control" type="email" name="email" />
+                </div>
+                <div class="form-row">
+                    <label class="form-label">Phone</label>
+                    <input class="form-control" type="tel" name="phone" />
+                </div>
+            </form>
+            <form name="form-events" class="form hide">
+                <div class="form-row">
+                    <label class="form-label">Title</label>
+                    <input class="form-control" type="text" name="title" />
+                </div>
+                <div class="form-row">
+                    <label class="form-label">Time</label>
+                    <input class="form-control" type="time" name="time" />
+                </div>
+                <div class="form-row">
+                    <label class="form-label">Address</label>
+                    <textarea class="form-control" name="address" ></textarea>
+                </div>
+            </form>
+            <div id="list-todo" class="todo hide"></div>
+      </div>
+  </body>
+</html>
